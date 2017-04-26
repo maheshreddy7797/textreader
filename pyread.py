0dcd3b9339b32b5ef1585aaef294bfcfe2531a87
Mahesh Reddy <maheshreddy7797@gmail.com>
2017-04-26 16:09:27 +0530

    Update pyread.py

--- pyread.py
+++ pyread.py
@@ -1,4 +1,14 @@
+import datetime
 print ("Hello CloudByte!!!")
 print ("I am reading text files\n")
-with open("/datadir/test.txt") as file:
-	print(file.read())
+with open("/datadir/input.txt") as file:
+	data = file.read()
+	print(data)
+	f= open("/datadir/output.txt","a")	
+	st=str(datetime.datetime.utcnow())
+	f.write("\n")
+	f.write(data)
+	print(st)	
+	f.write("Last executed on: ")
+	f.write(st)
+	f.close()
