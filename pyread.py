d58036ed4f61daae02cab5c054179691c586815a
Mahesh Reddy <maheshreddy7797@gmail.com>
2017-04-27 11:54:16 +0530

    Update pyread.py

--- pyread.py
+++ pyread.py
@@ -1,14 +1,12 @@
+"""This script reads a text file named as input.txt in user directory.
+   A new output.txt file will be created with log data."""
 import datetime
-print ("Hello CloudByte!!!")
-print ("I am reading text files\n")
-with open("/datadir/input.txt") as file:
-	data = file.read()
-	print(data)
-	f= open("/datadir/output.txt","a")	
-	st=str(datetime.datetime.utcnow())
-	f.write("\n")
-	f.write(data)
-	print(st)	
-	f.write("Last executed on: ")
-	f.write(st)
-	f.close()
+#Convert the standard timestamp into a string form
+TIME_STAMP = str(datetime.datetime.utcnow())
+#Open a file named input.txt from datadir in readmode
+with open('/datadir/input.txt') as file_1, \
+     open('/datadir/output.txt', 'w') as file_2:
+    file_2.write(file_1.read())
+#Write the time stamp content into a file
+    file_2.write("\nLast executed on: ")
+    file_2.write(TIME_STAMP)
